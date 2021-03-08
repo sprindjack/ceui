14fc1d9c8e88a5524a97942ff2f982677daf43a4
sprindjack <sprindjack@hotmail.com>
2021-03-08 21:57:20 +0800

    Update AutoSignMachine.js

--- AutoSignMachine.js
+++ AutoSignMachine.js
@@ -12,13 +12,8 @@ console.log = (function (oriLogFunc) {
   }
 })(console.log);
 
-// <!-- 在项目入口处加上如下代码 -->
-// process是Node 提供一些常用的全局变量，所有模块都可以调用
-console.log('Env:%o,:%o', process.env.NODE_ENV)   // production：生产环境  development：开发环境
-if (process.env.NODE_ENV === 'production') {
-  console = console || {};
-  console.log = function () { };
-}
+console = console || {};
+console.log = function () { };
 
 function registerEvn(argvs) {
   if (argvs.notify_sckey) {
